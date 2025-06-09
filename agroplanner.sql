fff5de895e041c4bf808bed28fb94d8dd5322c5b
joaolatczuk <joaocontatoscel@gmail.com>
2025-06-09 19:48:14 -0300

    Database

--- agroplanner.sql
+++ agroplanner.sql
@@ -1,110 +1,162 @@
-Updatecheck<|||>3<|||>1
-DoUsageStatistics<|||>3<|||>1
-UpdatecheckLastrun<|||>1<|||>2025-06-07 21:15:02
-LastUsageStatisticCall<|||>1<|||>2025-06-04 13:59:26
-LastSessions<|||>1<|||>XAMPP
-LastActiveSession<|||>1<|||>XAMPP
-ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
-ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
-ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
-ColSort_connform.ListSessions<|||>1<|||>0,0
-FileDialogEncoding_MainForm<|||>3<|||>0
-MainWinMaximized<|||>3<|||>0
-MainWinOnMonitor<|||>3<|||>0
-ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
-ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
-ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
-ColSort_MainForm.ListDatabases<|||>1<|||>0,0
-ColWidths_MainForm.ListVariables<|||>1<|||>160,200,316
-ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
-ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
-ColSort_MainForm.ListVariables<|||>1<|||>0,0
-ColWidths_MainForm.ListStatus<|||>1<|||>160,316,100,100
-ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
-ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
-ColSort_MainForm.ListStatus<|||>1<|||>0,0
-ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,186
-ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
-ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
-ColSort_MainForm.ListProcesses<|||>1<|||>0,1
-ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,256
-ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
-ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
-ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
-ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
-ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
-ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
-ColSort_MainForm.ListTables<|||>1<|||>0,0
-ColWidths_frmTableEditor.listColumns<|||>1<|||>29,100,154,90,60,65,50,117,130,140,100,100,50,50
-ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13
-ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13
-ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
-ColWidths_frmTableEditor.treeIndexes<|||>1<|||>181,100,80,120,80
-ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
-ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3,4
-ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
-ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>196,80,100,80,80,72
-ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
-ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
-ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
-ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,408
-ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
-ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
-ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
-MainWinLeft<|||>3<|||>676
-MainWinTop<|||>3<|||>0
-MainWinWidth<|||>3<|||>697
-MainWinHeight<|||>3<|||>727
-SessionManager_WindowLeft<|||>3<|||>581
-SessionManager_WindowTop<|||>3<|||>67
-querymemoheight<|||>3<|||>395
-SQLFile0<|||>1<|||>C:\Users\LATCZUK\Downloads\base-ilp540-202511.sql
-SQLFile1<|||>1<|||>C:\Users\LATCZUK\AppData\Roaming\HeidiSQL\Backups\query-tab-2025-06-07_22-02-42-254.sql
-SQLFile2<|||>1<|||>C:\Users\LATCZUK\Desktop\ES\agroplanner.sql
-CreateDbCollation<|||>1<|||>utf8mb4_general_ci
-DataFontSize<|||>3<|||>9
-Servers\XAMPP\SessionCreated<|||>1<|||>2025-06-04 13:59:29
-Servers\XAMPP\Host<|||>1<|||>127.0.0.1
-Servers\XAMPP\WindowsAuth<|||>3<|||>0
-Servers\XAMPP\CleartextPluginEnabled<|||>3<|||>0
-Servers\XAMPP\User<|||>1<|||>root
-Servers\XAMPP\Password<|||>1<|||>7
-Servers\XAMPP\LoginPrompt<|||>3<|||>0
-Servers\XAMPP\Port<|||>1<|||>3306
-Servers\XAMPP\NetType<|||>3<|||>0
-Servers\XAMPP\Compressed<|||>3<|||>0
-Servers\XAMPP\LocalTimeZone<|||>3<|||>0
-Servers\XAMPP\QueryTimeout<|||>3<|||>30
-Servers\XAMPP\KeepAlive<|||>3<|||>20
-Servers\XAMPP\FullTableStatus<|||>3<|||>1
-Servers\XAMPP\Databases<|||>1<|||>
-Servers\XAMPP\Library<|||>1<|||>libmariadb.dll
-Servers\XAMPP\Comment<|||>1<|||>
-Servers\XAMPP\StartupScriptFilename<|||>1<|||>
-Servers\XAMPP\TreeBackground<|||>3<|||>536870911
-Servers\XAMPP\SSHtunnelActive<|||>3<|||>0
-Servers\XAMPP\SshExecutable<|||>1<|||>
-Servers\XAMPP\SSHtunnelHost<|||>1<|||>
-Servers\XAMPP\SSHtunnelHostPort<|||>3<|||>22
-Servers\XAMPP\SSHtunnelUser<|||>1<|||>
-Servers\XAMPP\SSHtunnelPassword<|||>1<|||>6
-Servers\XAMPP\SSHtunnelTimeout<|||>3<|||>4
-Servers\XAMPP\SSHtunnelPrivateKey<|||>1<|||>
-Servers\XAMPP\SSHtunnelPort<|||>3<|||>3307
-Servers\XAMPP\SSL_Active<|||>3<|||>0
-Servers\XAMPP\SSL_Key<|||>1<|||>
-Servers\XAMPP\SSL_Cert<|||>1<|||>
-Servers\XAMPP\SSL_CA<|||>1<|||>
-Servers\XAMPP\SSL_Cipher<|||>1<|||>
-Servers\XAMPP\SSL_Verification<|||>3<|||>2
-Servers\XAMPP\IgnoreDatabasePattern<|||>1<|||>
-Servers\XAMPP\LogFileDdl<|||>3<|||>0
-Servers\XAMPP\LogFileDml<|||>3<|||>0
-Servers\XAMPP\LogFilePath<|||>1<|||>C:\Users\LATCZUK\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
-Servers\XAMPP\RefusedCount<|||>3<|||>7
-Servers\XAMPP\ServerVersionFull<|||>1<|||>10.4.32-MariaDB - mariadb.org binary distribution
-Servers\XAMPP\ConnectCount<|||>3<|||>10
-Servers\XAMPP\ServerVersion<|||>3<|||>100432
-Servers\XAMPP\LastConnect<|||>1<|||>2025-06-09 19:41:25
-Servers\XAMPP\lastUsedDB<|||>1<|||>agroplanner
-Servers\XAMPP\agroplanner|conteudos\MemoEditorHighlighter_status<|||>1<|||>
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.10.0.7000
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para agroplanner
+CREATE DATABASE IF NOT EXISTS `agroplanner` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `agroplanner`;
+
+-- Copiando estrutura para tabela agroplanner.categoriaepoca
+CREATE TABLE IF NOT EXISTS `categoriaepoca` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.categoriaepoca: ~7 rows (aproximadamente)
+INSERT INTO `categoriaepoca` (`id`, `nome`) VALUES
+	(1, 'Primavera'),
+	(2, 'Verão'),
+	(3, 'Outono'),
+	(4, 'Inverno'),
+	(5, 'Ano todo'),
+	(6, 'Estação seca'),
+	(7, 'Estação chuvosa');
+
+-- Copiando estrutura para tabela agroplanner.categoriasol
+CREATE TABLE IF NOT EXISTS `categoriasol` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.categoriasol: ~6 rows (aproximadamente)
+INSERT INTO `categoriasol` (`id`, `nome`) VALUES
+	(1, 'Pleno sol – mínimo de 4h de sol direto'),
+	(2, 'Meia sombra – algumas horas com luz direta'),
+	(3, 'Sombra – luz indireta, sem sol direto'),
+	(4, 'Sol pleno o dia todo'),
+	(5, 'Sol da manhã e sombra à tarde'),
+	(6, 'Sombra total – ambiente interno');
+
+-- Copiando estrutura para tabela agroplanner.categoriasolo
+CREATE TABLE IF NOT EXISTS `categoriasolo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.categoriasolo: ~8 rows (aproximadamente)
+INSERT INTO `categoriasolo` (`id`, `nome`) VALUES
+	(1, 'Arenoso'),
+	(2, 'Argiloso'),
+	(3, 'Siltoso'),
+	(4, 'Humoso'),
+	(5, 'Calcário'),
+	(6, 'Gessado'),
+	(7, 'Alagado'),
+	(8, 'Mistura de solo (composto)');
+
+-- Copiando estrutura para tabela agroplanner.conteudocategoria
+CREATE TABLE IF NOT EXISTS `conteudocategoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.conteudocategoria: ~8 rows (aproximadamente)
+INSERT INTO `conteudocategoria` (`id`, `nome`) VALUES
+	(1, 'Flor'),
+	(2, 'Fruta'),
+	(3, 'Legume'),
+	(4, 'Verdura'),
+	(5, 'Erva'),
+	(6, 'Raiz'),
+	(7, 'Cereal'),
+	(8, 'Trepadeira');
+
+-- Copiando estrutura para tabela agroplanner.conteudos
+CREATE TABLE IF NOT EXISTS `conteudos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomePlanta` varchar(100) NOT NULL,
+  `categoria` varchar(50) NOT NULL,
+  `epoca` varchar(50) NOT NULL,
+  `temperatura` varchar(20) DEFAULT NULL,
+  `solo` text DEFAULT NULL,
+  `rega` varchar(50) DEFAULT NULL,
+  `sol` varchar(50) DEFAULT NULL,
+  `instrucoes` text NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `data_publicacao` datetime DEFAULT current_timestamp(),
+  `status` varchar(20) DEFAULT 'pendente',
+  `favorita` tinyint(1) DEFAULT 0,
+  `statusAtivo` tinyint(4) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `id_autor` (`id_autor`),
+  CONSTRAINT `conteudos_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.conteudos: ~5 rows (aproximadamente)
+INSERT INTO `conteudos` (`id`, `nomePlanta`, `categoria`, `epoca`, `temperatura`, `solo`, `rega`, `sol`, `instrucoes`, `id_autor`, `data_publicacao`, `status`, `favorita`, `statusAtivo`) VALUES
+	(8, '123', 'Legume', 'Inverno', '3', 'Argiloso', '4 semana', 'Pleno sol – mínimo de 4h de sol direto', '123', 1, '2025-06-07 16:35:53', 'aprovado', 0, 1),
+	(9, '123', 'Fruta', 'Primavera', '6', 'Siltoso', '5 semana', 'Meia sombra – algumas horas com luz direta', '123', 1, '2025-06-07 16:58:31', 'rejeitado', 0, 1),
+	(10, '123', 'Fruta', 'Verão', '5', 'Argiloso', '4 dia', 'Meia sombra – algumas horas com luz direta', '123', 1, '2025-06-07 17:00:31', 'pendente', 0, 0),
+	(11, '123', 'Flor', 'Primavera', '8', 'Arenoso', '8 dia', 'Sol pleno o dia todo', '123', 1, '2025-06-08 11:54:51', 'pendente', 0, 1),
+	(12, '123', 'Fruta', 'Verão', '22', 'Humoso', '7 semana', 'Sombra – luz indireta, sem sol direto', '3312ljhpiahpauPIUHPI', 1, '2025-06-08 11:55:26', 'pendente', 0, 1),
+	(13, '123', 'Raiz', 'Verão', '13', 'Humoso', '4 mês', 'Pleno sol – mínimo de 4h de sol direto', 'IODUOIUiouaposuiipsauoOIUP', 1, '2025-06-08 11:56:00', 'aprovado', 0, 1);
+
+-- Copiando estrutura para tabela agroplanner.imagens_conteudo
+CREATE TABLE IF NOT EXISTS `imagens_conteudo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_conteudo` int(11) NOT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_conteudo` (`id_conteudo`),
+  CONSTRAINT `imagens_conteudo_ibfk_1` FOREIGN KEY (`id_conteudo`) REFERENCES `conteudos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.imagens_conteudo: ~5 rows (aproximadamente)
+INSERT INTO `imagens_conteudo` (`id`, `id_conteudo`, `url`) VALUES
+	(6, 8, '/uploads/1749324953021-chuchu.jpg'),
+	(7, 9, '/uploads/1749326311535-plantar.png'),
+	(8, 10, '/uploads/1749326431095-no-star.png'),
+	(9, 11, '/uploads/1749394491423-girassol-2-scaled.jpg'),
+	(10, 12, '/uploads/1749394526604-sementes_de_margarida_gigante_branca_1467_sementes_2513_1_1f0e3931c1faf127987e5d683f6e9f2b.jpg'),
+	(11, 13, '/uploads/1749394560026-Papoula.jpg');
+
+-- Copiando estrutura para tabela agroplanner.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo` enum('comum','admin') DEFAULT 'comum',
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.usuarios: ~3 rows (aproximadamente)
+INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`, `criado_em`) VALUES
+	(1, 'João Vinicius Latczuk', 'joao.latczuk@fatec.sp.gov.br', '123', 'admin', '2025-05-11 18:06:43'),
+	(2, 'Marlene Aparecida Silva', 'marleneescola77@gmail.com', '123', 'comum', '2025-05-11 21:37:09'),
+	(3, 'João Vinicius Latczuk', 'joaocontascel@gmail.com', '123', 'comum', '2025-05-13 01:27:41');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
