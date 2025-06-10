785a0acbca1a744122a2b4fe8b55a587f9596ea5
joaolatczuk <joaocontatoscel@gmail.com>
2025-06-10 00:29:35 -0300

    Database

--- agroplanner.sql
+++ agroplanner.sql
@@ -19,6 +19,24 @@
 CREATE DATABASE IF NOT EXISTS `agroplanner` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `agroplanner`;
 
+-- Copiando estrutura para tabela agroplanner.conteudocategoria
+CREATE TABLE IF NOT EXISTS `conteudocategoria` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.conteudocategoria: ~8 rows (aproximadamente)
+INSERT IGNORE INTO `conteudocategoria` (`id`, `nome`) VALUES
+	(1, 'Flor'),
+	(2, 'Fruta'),
+	(3, 'Legume'),
+	(4, 'Verdura'),
+	(5, 'Erva'),
+	(6, 'Raiz'),
+	(7, 'Cereal'),
+	(8, 'Trepadeira');
+
 -- Copiando estrutura para tabela agroplanner.conteudoepoca
 CREATE TABLE IF NOT EXISTS `conteudoepoca` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -27,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `conteudoepoca` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela agroplanner.conteudoepoca: ~7 rows (aproximadamente)
-INSERT INTO `conteudoepoca` (`id`, `nome`) VALUES
+INSERT IGNORE INTO `conteudoepoca` (`id`, `nome`) VALUES
 	(1, 'Primavera'),
 	(2, 'Verão'),
 	(3, 'Outono'),
@@ -35,6 +53,44 @@ INSERT INTO `conteudoepoca` (`id`, `nome`) VALUES
 	(5, 'Ano todo'),
 	(6, 'Estação seca'),
 	(7, 'Estação chuvosa');
+
+-- Copiando estrutura para tabela agroplanner.conteudos
+CREATE TABLE IF NOT EXISTS `conteudos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nomePlanta` varchar(100) NOT NULL,
+  `temperatura` varchar(20) DEFAULT NULL,
+  `rega` varchar(50) DEFAULT NULL,
+  `instrucoes` text NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL,
+  `id_epoca` int(11) DEFAULT NULL,
+  `id_solo` int(11) DEFAULT NULL,
+  `id_sol` int(11) DEFAULT NULL,
+  `data_publicacao` datetime DEFAULT current_timestamp(),
+  `status` varchar(20) DEFAULT 'pendente',
+  `statusAtivo` tinyint(4) DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `id_autor` (`id_autor`),
+  KEY `id_categoria` (`id_categoria`),
+  KEY `id_epoca` (`id_epoca`),
+  KEY `id_solo` (`id_solo`),
+  KEY `id_sol` (`id_sol`),
+  CONSTRAINT `conteudos_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `conteudos_ibfk_2` FOREIGN KEY (`id_categoria`) REFERENCES `conteudocategoria` (`id`),
+  CONSTRAINT `conteudos_ibfk_3` FOREIGN KEY (`id_epoca`) REFERENCES `conteudoepoca` (`id`),
+  CONSTRAINT `conteudos_ibfk_4` FOREIGN KEY (`id_solo`) REFERENCES `conteudosolo` (`id`),
+  CONSTRAINT `conteudos_ibfk_5` FOREIGN KEY (`id_sol`) REFERENCES `conteudosol` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela agroplanner.conteudos: ~7 rows (aproximadamente)
+INSERT IGNORE INTO `conteudos` (`id`, `nomePlanta`, `temperatura`, `rega`, `instrucoes`, `id_autor`, `id_categoria`, `id_epoca`, `id_solo`, `id_sol`, `data_publicacao`, `status`, `statusAtivo`) VALUES
+	(1, '123', '3', '4 semana', '123', 1, 3, 4, 2, 1, '2025-06-07 16:35:53', 'aprovado', 1),
+	(2, '123', '6', '5 semana', '123', 1, 2, 1, 3, 2, '2025-06-07 16:58:31', 'aprovado', 1),
+	(3, '123', '5', '4 dia', '123', 1, 2, 2, 2, 2, '2025-06-07 17:00:31', 'aprovado', 1),
+	(4, '123', '8', '8 dia', '123', 1, 1, 1, 1, 4, '2025-06-08 11:54:51', 'aprovado', 1),
+	(5, '123', '22', '7 semana', '3312ljhpiahpauPIUHPI', 1, 2, 2, 4, 3, '2025-06-08 11:55:26', 'aprovado', 1),
+	(6, '123', '13', '4 mês', 'IODUOIUiouaposuiipsauoOIUP', 1, 6, 2, 4, 1, '2025-06-08 11:56:00', 'aprovado', 1),
+	(9, '123', '6', '8 semana', '123321', 1, 4, 1, 4, 6, '2025-06-09 23:15:48', 'aprovado', 1);
 
 -- Copiando estrutura para tabela agroplanner.conteudosol
 CREATE TABLE IF NOT EXISTS `conteudosol` (
@@ -44,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `conteudosol` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela agroplanner.conteudosol: ~6 rows (aproximadamente)
-INSERT INTO `conteudosol` (`id`, `nome`) VALUES
+INSERT IGNORE INTO `conteudosol` (`id`, `nome`) VALUES
 	(1, 'Pleno sol – mínimo de 4h de sol direto'),
 	(2, 'Meia sombra – algumas horas com luz direta'),
 	(3, 'Sombra – luz indireta, sem sol direto'),
@@ -60,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `conteudosolo` (
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela agroplanner.conteudosolo: ~8 rows (aproximadamente)
-INSERT INTO `conteudosolo` (`id`, `nome`) VALUES
+INSERT IGNORE INTO `conteudosolo` (`id`, `nome`) VALUES
 	(1, 'Arenoso'),
 	(2, 'Argiloso'),
 	(3, 'Siltoso'),
@@ -70,54 +126,6 @@ INSERT INTO `conteudosolo` (`id`, `nome`) VALUES
 	(7, 'Alagado'),
 	(8, 'Mistura de solo (composto)');
 
--- Copiando estrutura para tabela agroplanner.conteudocategoria
-CREATE TABLE IF NOT EXISTS `conteudocategoria` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Copiando dados para a tabela agroplanner.conteudocategoria: ~8 rows (aproximadamente)
-INSERT INTO `conteudocategoria` (`id`, `nome`) VALUES
-	(1, 'Flor'),
-	(2, 'Fruta'),
-	(3, 'Legume'),
-	(4, 'Verdura'),
-	(5, 'Erva'),
-	(6, 'Raiz'),
-	(7, 'Cereal'),
-	(8, 'Trepadeira');
-
--- Copiando estrutura para tabela agroplanner.conteudos
-CREATE TABLE IF NOT EXISTS `conteudos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nomePlanta` varchar(100) NOT NULL,
-  `categoria` varchar(50) NOT NULL,
-  `epoca` varchar(50) NOT NULL,
-  `temperatura` varchar(20) DEFAULT NULL,
-  `solo` text DEFAULT NULL,
-  `rega` varchar(50) DEFAULT NULL,
-  `sol` varchar(50) DEFAULT NULL,
-  `instrucoes` text NOT NULL,
-  `id_autor` int(11) NOT NULL,
-  `data_publicacao` datetime DEFAULT current_timestamp(),
-  `status` varchar(20) DEFAULT 'pendente',
-  `favorita` tinyint(1) DEFAULT 0,
-  `statusAtivo` tinyint(4) DEFAULT 1,
-  PRIMARY KEY (`id`),
-  KEY `id_autor` (`id_autor`),
-  CONSTRAINT `conteudos_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Copiando dados para a tabela agroplanner.conteudos: ~5 rows (aproximadamente)
-INSERT INTO `conteudos` (`id`, `nomePlanta`, `categoria`, `epoca`, `temperatura`, `solo`, `rega`, `sol`, `instrucoes`, `id_autor`, `data_publicacao`, `status`, `favorita`, `statusAtivo`) VALUES
-	(8, '123', 'Legume', 'Inverno', '3', 'Argiloso', '4 semana', 'Pleno sol – mínimo de 4h de sol direto', '123', 1, '2025-06-07 16:35:53', 'aprovado', 0, 1),
-	(9, '123', 'Fruta', 'Primavera', '6', 'Siltoso', '5 semana', 'Meia sombra – algumas horas com luz direta', '123', 1, '2025-06-07 16:58:31', 'rejeitado', 0, 1),
-	(10, '123', 'Fruta', 'Verão', '5', 'Argiloso', '4 dia', 'Meia sombra – algumas horas com luz direta', '123', 1, '2025-06-07 17:00:31', 'pendente', 0, 0),
-	(11, '123', 'Flor', 'Primavera', '8', 'Arenoso', '8 dia', 'Sol pleno o dia todo', '123', 1, '2025-06-08 11:54:51', 'pendente', 0, 1),
-	(12, '123', 'Fruta', 'Verão', '22', 'Humoso', '7 semana', 'Sombra – luz indireta, sem sol direto', '3312ljhpiahpauPIUHPI', 1, '2025-06-08 11:55:26', 'pendente', 0, 1),
-	(13, '123', 'Raiz', 'Verão', '13', 'Humoso', '4 mês', 'Pleno sol – mínimo de 4h de sol direto', 'IODUOIUiouaposuiipsauoOIUP', 1, '2025-06-08 11:56:00', 'aprovado', 0, 1);
-
 -- Copiando estrutura para tabela agroplanner.imagens_conteudo
 CREATE TABLE IF NOT EXISTS `imagens_conteudo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -126,16 +134,11 @@ CREATE TABLE IF NOT EXISTS `imagens_conteudo` (
   PRIMARY KEY (`id`),
   KEY `id_conteudo` (`id_conteudo`),
   CONSTRAINT `imagens_conteudo_ibfk_1` FOREIGN KEY (`id_conteudo`) REFERENCES `conteudos` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Copiando dados para a tabela agroplanner.imagens_conteudo: ~5 rows (aproximadamente)
-INSERT INTO `imagens_conteudo` (`id`, `id_conteudo`, `url`) VALUES
-	(6, 8, '/uploads/1749324953021-chuchu.jpg'),
-	(7, 9, '/uploads/1749326311535-plantar.png'),
-	(8, 10, '/uploads/1749326431095-no-star.png'),
-	(9, 11, '/uploads/1749394491423-girassol-2-scaled.jpg'),
-	(10, 12, '/uploads/1749394526604-sementes_de_margarida_gigante_branca_1467_sementes_2513_1_1f0e3931c1faf127987e5d683f6e9f2b.jpg'),
-	(11, 13, '/uploads/1749394560026-Papoula.jpg');
+-- Copiando dados para a tabela agroplanner.imagens_conteudo: ~1 rows (aproximadamente)
+INSERT IGNORE INTO `imagens_conteudo` (`id`, `id_conteudo`, `url`) VALUES
+	(3, 9, '/uploads/1749521748939-vecteezy_gradient-paper-cut-style-background-with-gray-colours_26562701.png');
 
 -- Copiando estrutura para tabela agroplanner.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -150,10 +153,10 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Copiando dados para a tabela agroplanner.usuarios: ~3 rows (aproximadamente)
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`, `criado_em`) VALUES
-	(1, 'João Vinicius Latczuk', 'joao.latczuk@fatec.sp.gov.br', '123', 'admin', '2025-05-11 18:06:43'),
-	(2, 'Marlene Aparecida Silva', 'marleneescola77@gmail.com', '123', 'comum', '2025-05-11 21:37:09'),
-	(3, 'João Vinicius Latczuk', 'joaocontascel@gmail.com', '123', 'comum', '2025-05-13 01:27:41');
+INSERT IGNORE INTO `usuarios` (`id`, `nome`, `email`, `senha`, `tipo`, `criado_em`) VALUES
+	(1, 'João Vinicius Latczuk', 'joao.latczuk@fatec.sp.gov.br', '123', 'admin', '2025-05-11 21:06:43'),
+	(2, 'Marlene Aparecida Silva', 'marleneescola77@gmail.com', '123', 'comum', '2025-05-12 00:37:09'),
+	(3, 'João Vinicius Latczuk', 'joaocontascel@gmail.com', '123', 'comum', '2025-05-13 04:27:41');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
